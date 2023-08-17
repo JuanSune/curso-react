695c310aeee56dd0cc7acacee8f76227fcd1b69a
Juan Suñe <111535673+JuanSune@users.noreply.github.com>
2023-08-17 13:24:34 -0300

    g

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -1,11 +1,13 @@
 
 import './App.css';
+import Hello from './components/HelloWorld';
 
 function App() {
   return (
     <div className="App">
       <h1>Ola gente, como vai?</h1>
       <p>Meu primeiro código?</p>
+      <Hello/>
     </div>
   );
 }
--- meu-projeto/src/components/HelloWorld.js
+++ meu-projeto/src/components/HelloWorld.js
@@ -1,5 +1,10 @@
 function HellWorld(){
-
+    return(
+        <div>
+            <h1>World</h1>
+        </div>
+    )     
+    
 }
 
 export default HellWorld;
