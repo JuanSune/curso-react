608bcbfb859ed6d0a63e3e3f1823f4157c7a000d
Juan Suñe <111535673+JuanSune@users.noreply.github.com>
2023-08-20 22:25:32 -0300

    sj

--- meu-projeto/src/components/condicional/Condicional.jsx
+++ meu-projeto/src/components/condicional/Condicional.jsx
@@ -1,7 +1,21 @@
 //Aqui e como faz a condiocional no REACT
 
-function condicional(){
+import { userState } from 'react' 
 
+function condicional(){
+    const [ name, setName ] = userState()
+    return(
+        <div>
+            <form>
+                <div>
+                    
+                </div>
+                <div>
+
+                </div>
+            </form>
+        </div>
+    )
 }
 
 export default condicional;
