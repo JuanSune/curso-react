e5210af8e38f9a26cd2080aa0dff6046f37aed3d
Juan Suñe <111535673+JuanSune@users.noreply.github.com>
2023-08-21 05:17:15 -0300

    final

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -1,9 +1,36 @@
 import "./App.css"
+import { BrowserRouter as Router,Link,Routes,Route } from "react-router-dom" 
+import Contato from "./pages/Contato"
+import Empresa from "./pages/Empresa"
+import Home from "./pages/Home"
+import Navbar from "./pages/Navbar"
+
+
+//Existem varias formas de fazer algo em HTML, tanto usando React JS 
+//ou usando Express, USE COM SABEDORIA -- Não se esqueça do mongoose - 
+//mongoDB
 
 function App() {
   return (
     <div className="App">
-      <h1>State Lift</h1>
+      <Router>
+      <Navbar/>
+      <Routes>
+          <Route path="/Contato"
+          element={<Contato />}>
+            
+          </Route>
+          <Route path="/Empresa"
+          element={<Empresa />}>
+            
+          </Route>
+          <Route exact path="/" element={<Home />}>
+            
+          </Route>
+      </Routes>
+      </Router>
+    
+
     </div>
   )
 }
